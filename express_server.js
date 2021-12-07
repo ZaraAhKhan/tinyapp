47f742e00ec17ce2c8c38146c7783480610ecd00
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-12-06 19:53:56 -0500

    adding a form to be displayed when a get request is made

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 //set ejs as a view engine
 app.set('view engine','ejs');
@@ -23,11 +25,20 @@ app.get('/hello',(req,res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+//adding a GET route to display the form
+app.get('/urls/new',(req,res) => {
+  res.render('urls_new');
+});
+
 //adding a second route using route parameter
 app.get('/urls/:shortURL', (req,res) => {
   const templateVariable = {shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
   res.render("urls_show" , templateVariable);
-})
+});
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
